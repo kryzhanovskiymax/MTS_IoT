44eba2d36e320b0da85a5b70835dcd3bb9b79c92
arinenn <arinenn@ya.ru>
2022-04-09 17:08:34 +0300

    functionable game

--- client/src/script.js
+++ client/src/script.js
@@ -1,23 +1,41 @@
-let time_change = 0.5;
+let time_change = 500;
 let index_img = 0;
+let counter = 0;
+let timerID;
+
+const circle = '<svg><circle cx="150" cy="150" r="100"/></svg>'
+const square = '<svg><rect x="50" y="40" width="200" height="200"/></svg>'
+const triangle = '<svg><polygon points="150 40, 70 220, 230 220"/></svg>'
 
 const element = document.getElementById('test_img_hole')
+const images = [1, 2, 3, 3, 2, 1, 2, 1, 2, 3, 3, 2, 1, 2]
 
-function changeImages(index_img) {
-// images = [1, 2, 3, 2, 2, 1...]
-    switch (index_img) {
-        case 1: element.style.backgroundImage = "url('./imgs/triangle.png')"
-        case 2: element.style.backgroundImage = "url('./imgs/square.png')"
-        case 3: element.style.backgroundImage = "url('./imgs/circle.png')"
+function changeImages() {
+    console.log('WIN')
+    switch (images[index_img]) {
+        case 1: {element.innerHTML = circle; break}
+        case 2: {element.innerHTML = square; break}
+        case 3: {element.innerHTML = triangle; break}
+    }
+    index_img += 1
+
+    if (index_img === images.length - 1) {
+        clearInterval(timerID)
     }
 }
 
-btn_1.onclick = function() {
-    console.log('WIN');
-    document.body.style.backgroundColor = 'red'
-    element.style.backgroundImage = "url('./imgs/square.png')"
+btn_run.onclick = game = () => {
+    timerID = setInterval(changeImages, time_change, index_img)
 }
 
-btn_run.onclick = function() {
+btn_1.onclick = function() {
+
+}
+
+btn_2.onclick = function() {
+    
+}
+
+btn_3.onclick = function() {
 
 }
