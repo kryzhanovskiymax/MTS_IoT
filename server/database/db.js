b3877457458e481227916f5763c4b9daaacf972f
Max Kryzhanovskiy <kryzhanovskiymax@mail.ru>
2022-04-10 07:15:46 +0300

    env variable add

--- server/database/db.js
+++ server/database/db.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    user: 'makskryzhanovskiy',
-    host: 'localhost',
-    database: 'makskryzhanovskiy',
-    password: 'password',
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
     port: 5432
 });
 
